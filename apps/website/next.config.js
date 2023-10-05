170fe1c327e4365f6c92e8b6831479f919624afe
Oleksandr Fediashov <olfedias@microsoft.com>
2023-10-05 12:48:38 +0200

    docs: setup website and add boilterplate (#8)

---
+++ apps/website/next.config.js
@@ -0,0 +1,10 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+});
+
+module.exports = withNextra({
+  images: {
+    unoptimized: true,
+  },
+});
---
+++ apps/website/theme.config.js
@@ -0,0 +1,18 @@
+export default {
+  footer: {
+    component: null,
+  },
+  logo: <span>WyW-in-JS</span>,
+  primaryHue: 210,
+  primarySaturation: 100,
+  project: {
+    docsRepositoryBase:
+      'https://github.com/Anber/wyw-in-js/tree/main/apps/website',
+    link: 'https://github.com/Anber/wyw-in-js',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – WyW-in-JS',
+    };
+  },
+};
