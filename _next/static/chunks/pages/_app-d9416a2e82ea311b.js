db94c1e52148ee888930ddb90b7ed9c1fe189010
Anber <anton@evz.name>
2024-05-23 15:27:43 +0000

    Deploying to gh-pages from @ Anber/wyw-in-js@b407f83d1c9b4a2dfc26e286798b720cb54507cb 🚀

--- _next/static/chunks/pages/_app-d9416a2e82ea311b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{9831:function(_,n,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return e(8731)}])}},function(_){var __webpack_exec__=function(n){return _(_.s=n)};_.O(0,[774,179],function(){return __webpack_exec__(9831),__webpack_exec__(4456)}),_N_E=_.O()}]);
