6f93f605ca6fb665759390eab95f9e2198ef1657
Anton Evzhakov <anton@evz.name>
2026-04-20 20:51:52 +0300

    fix(cli): remove stale jest preset reference

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -4,8 +4,13 @@
  * @type {import('@jest/types').Config.InitialOptions}
  */
 module.exports = {
+  cacheDirectory: '<rootDir>/node_modules/.cache/jest',
+  clearMocks: true,
   displayName: 'cli',
-  preset: '@wyw-in-js/jest-preset',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/esm/', '/lib/', '/types/'],
   transform: {
     '^.+\\.ts$': [
       'ts-jest',
